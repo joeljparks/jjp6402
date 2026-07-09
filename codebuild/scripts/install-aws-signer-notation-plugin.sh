e088abd3a4852d379d95bdd6e9ac610866ad44c3
joeljparks <joeljparks@Joels-MBP.parks.lan>
2026-07-09 05:31:53 -0500

    I am questioning my life choices.  Fix for dumb typo in AWS Signer config.  FFS

--- codebuild/scripts/install-aws-signer-notation-plugin.sh
+++ codebuild/scripts/install-aws-signer-notation-plugin.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
-TMP=$(mktemp -d)
-curl -fsSLo "$TMP/plugin.zip" "https://d2hvyiie56hcat.cloudfront.net/linux/amd64/installer.zip"
-unzip -o "$TMP/plugin.zip" -d "$TMP"
-bash "$TMP/install.sh"
-notation plugin ls || true
+
+ARCH_RAW="$(uname -m)"
+case "$ARCH_RAW" in
+  x86_64|amd64)
+    ARCH="amd64"
+    ;;
+  aarch64|arm64)
+    ARCH="arm64"
+    ;;
+  *)
+    echo "Unsupported architecture: ${ARCH_RAW}" >&2
+    exit 1
+    ;;
+esac
+
+TMP="$(mktemp -d)"
+PKG="${TMP}/aws-signer-notation-cli_${ARCH}.deb"
+URL="https://d2hvyiie56hcat.cloudfront.net/linux/${ARCH}/installer/deb/latest/aws-signer-notation-cli_${ARCH}.deb"
+
+echo "Installing AWS Signer Notation package from official AWS package URL."
+curl --fail --location --show-error --silent --retry 3 --retry-delay 2 -o "$PKG" "$URL"
+
+dpkg -i -E "$PKG"
+
+notation version
+notation plugin ls
+notation plugin ls | grep -q 'com.amazonaws.signer.notation.plugin'
--- environment/signing/main.tf
+++ environment/signing/main.tf
@@ -1,7 +1,7 @@
 variable "name_prefix" { type = string }
 
 resource "aws_signer_signing_profile" "container" {
-  name_prefix = "jjp6402_container_"
+  name        = "jjp6402_container"
   platform_id = "Notation-OCI-SHA384-ECDSA"
 }
 
